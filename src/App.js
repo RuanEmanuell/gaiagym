492694efbbe4e7924370be2473a78e3df817e7f9
Ruan Emanuell Alencar <ruanvitorcom540@gmail.com>
2024-03-29 15:28:50 -0300

    (Version 1.0) Starting landing content

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import { ReactComponent as MenuIcon } from "./images/menu.svg";
+import GymBackground from "./images/gym.png";
+import Plan1Img from "./images/fitplan.jpg";
+import Plan2Img from "./images/smartplan.jpeg";
 import './App.css';
 
 function App() {
@@ -7,15 +10,46 @@ function App() {
     <div>
       <nav className="navbar">
         <section className="navbarContent">
-          <h1 className="navbarTitle">GaiaGym</h1>
+          <h1 className="navbarTitle">GAIAGYM</h1>
           <MenuIcon className="navbarIcon" />
           <div className="navbarOptions">
             <h2>Início</h2>
-            <h2>Sessões</h2>
+            <h2>Sobre</h2>
             <h2>Contato</h2>
           </div>
         </section>
       </nav>
+      <div className="container">
+        <main>
+          <h1 className="title">GAIAGYM</h1>
+          <h2 className="subtitle">A academia que você estava procurando</h2>
+          <article className="prices">
+            <div className="priceBox">
+              <h3 className="priceTitle">
+                Plano Fit
+              </h3>
+              <img src={Plan1Img} className="priceImg"></img>
+              <p className="priceDescription">Treine quando quiser</p>
+              <h3 className="priceTitle">
+                A partir de R$ 89,99
+              </h3>
+              <button className="standartButton">Escolher</button>
+            </div>
+            <div className="priceBox">
+              <h3 className="priceTitle">
+                Plano Smart
+              </h3>
+              <img src={Plan2Img} className="priceImg"></img>
+              <p className="priceDescription">Treine acompanhado por um de nossos profissionais</p>
+              <h3 className="priceTitle">
+                A partir de R$ 129,99
+              </h3>
+              <button className="standartButton">Escolher</button>
+            </div>
+          </article>
+        </main>
+        <img className="containerBackground" src={GymBackground}></img>
+      </div>
     </div>
   );
 }

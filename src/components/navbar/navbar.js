29a9ff0de707f80e598cebce4b39d8872302dd69
Ruan Emanuell Alencar <ruanvitorcom540@gmail.com>
2024-03-30 10:58:32 -0300

    (Version 1.0) Contact screen

--- src/components/navbar/navbar.js
+++ src/components/navbar/navbar.js
@@ -1,21 +1,43 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import { ReactComponent as MenuIcon } from "../../images/menu.svg";
 import "../../App.css";
 import "./navbar.css";
 
 function Navbar() {
+  const dialogRef = useRef(null);
+  function openMobileMenu() {
+    dialogRef.current.showModal();
+  }
+  function closeMobileMenu() {
+    dialogRef.current.close();
+  }
+
+  function scrollTo(scrollElement) {
+    const scrollPosition = document.querySelector(`.${scrollElement}`).offsetTop;
+    window.scrollTo({
+      top: scrollPosition,
+      behavior: "smooth"
+    })
+  }
   return (
     <nav className="navbar">
-    <section className="navbarContent">
-      <h1 className="navbarTitle">GAIAGYM</h1>
-      <MenuIcon className="navbarIcon" />
-      <div className="navbarOptions">
-        <h2>Início</h2>
-        <h2>Sobre</h2>
-        <h2>Contato</h2>
-      </div>
-    </section>
-  </nav>
+      <section className="navbarContent">
+        <h1 className="navbarTitle">GAIAGYM</h1>
+        <MenuIcon className="navbarIcon" onClick={openMobileMenu} />
+        <div className="navbarOptions">
+          <h2 onClick={() => scrollTo("home")}>Início</h2>
+          <h2 onClick={() => scrollTo("about")}>Sobre</h2>
+          <h2 onClick={() => scrollTo("contact")}>Contato</h2>
+        </div>
+      </section>
+      <dialog className="navbarMobileMenuBox" ref={dialogRef} onClick={closeMobileMenu}>
+        <div className="navbarMobileMenu">
+          <h2 onClick={() => scrollTo("home")}>Início</h2>
+          <h2 onClick={() => scrollTo("about")}>Sobre</h2>
+          <h2 onClick={() => scrollTo("contact")}>Contato</h2>
+        </div>
+      </dialog>
+    </nav>
   );
 }
 
--- src/pages/contact/contact.js
+++ src/pages/contact/contact.js
@@ -1,5 +1,8 @@
 import React from 'react';
-import GymBackground from "../../images/gym.png";
+import GymBackground from "../../images/gym2.png";
+import { ReactComponent as WhatsappIcon } from "../../images/whatsapp.svg";
+import { ReactComponent as InstagramIcon } from "../../images/instagram.svg";
+import { ReactComponent as GooglePlayIcon } from "../../images/googleplay.svg";
 import "../../App.css";
 import "./contact.css";
 
@@ -8,6 +11,17 @@ function Contact() {
     <div className="contact container">
       <main>
         <h1 className="title">Contato</h1>
+        <div className="contactGrid">
+          <div className="contactMapBox">
+            <iframe className="contactMap" src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d15020.783548003294!2d-47.957493659155276!3d-19.746830374961384!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sen!2sbr!4v1711804202414!5m2!1sen!2sbr" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+            <p>Venha nos visitar!</p>
+          </div>
+          <div className="contactButtonsBox">
+            <button className="contactButton" style={{backgroundColor:"#14c700"}}><WhatsappIcon className="contactButtonIcon"/>Whatsapp</button>
+            <button className="contactButton" style={{backgroundColor:"#dd2a7b"}}><InstagramIcon className="contactButtonIcon"/>Instagram</button>
+            <button className="contactButton" style={{backgroundColor:"#000000"}}><GooglePlayIcon className="contactButtonIcon"/>Nosso app</button>
+          </div>
+        </div>
       </main>
       <img className="containerBackground" src={GymBackground}></img>
     </div>
